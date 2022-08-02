5550023c0bae92d9d43ce5db518488423f5ac430
Alexander Voinov <eji001@mail.ru>
2022-08-02 22:07:20 +0300

    add ex005 if-else

---
+++ Ex005_name/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if (username.ToLower() =="маша"){
+    Console.WriteLine("ура, этоже " + username);
+}
+else{
+    Console.WriteLine("привет " + username);
+}
