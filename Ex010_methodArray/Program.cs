f690365c6b7878a7a8b12bb4aa08737d2c4ac230
Alexander Voinov <eji001@mail.ru>
2022-08-07 16:30:11 +0300

    add ex010 (работа с масивами ч2)

---
+++ Ex010_methodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿//массивы
+
+int[] array = { 1, 12, 31, 4, 15, 4, 17, 18 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
