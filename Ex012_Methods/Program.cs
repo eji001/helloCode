4cc59ba3a93eb6371c82de0761c33eaeb3a94601
Alexander Voinov <eji001@mail.ru>
2022-08-13 15:19:50 +0300

    add ex012 (виды методов)

--- Ex012_Methods/Program.cs
+++ Ex012_Methods/Program.cs
@@ -70,3 +70,12 @@ string Method4(int count, string text)
 
 string res = Method4(10, "z");
 Console.WriteLine(res);
+
+for (int i=2; i<=10;i++)
+{
+    for (int j=0; j<=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
